4448ee0c542e54b89851d445e8912a19c0173610
user <user@LAPTOP-74O8CNB2.com>
2025-04-24 07:00:03 +0800

    contact

--- script.js
+++ script.js
@@ -11,11 +11,35 @@ $(document).ready(function () {
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if (this.scrollY > 1500) {
+            $('.scroll-up-btn').addClass("lols");
+        }else{
+            $('.scroll-up-btn').removeClass("lols");
+        }
     });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    })
+
+    var typed = new Typed ("#typed", {
+        strings: ["Frontend Developer", "Freelancer", "Vlogger", "Youtuber"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+
+    var typed = new Typed (".typing", {
+        strings: ["Frontend Developer", "Freelancer", "Vlogger", "Youtuber"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
     })
 
     $('.carousel').owlCarousel({
